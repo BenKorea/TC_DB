2d61f61a1ebe9a590e438989b4df602a0026c35a
BenKorea <kimbi.kirams@gmail.com>
2025-01-23 20:16:35 +0900

    desktop

--- source/my_parsing_Tx_EMR.R
+++ source/my_parsing_Tx_EMR.R
@@ -1,11 +1,11 @@
 ################################################################################
 ## my_functions_for_emr_tx
 ################################################################################
-my_splilt_lines_for_emr_tx <- function(dt) {
+my_splilt_lines_for_Tx_EMR <- function(dt) {
   
   dt[, line_count := str_count(특기사항, "\n") + 1]
   if (input_error_checking_mode == "Y") {
-    emr_tx_line_count_errors <<- dt[line_count >= 3]
+    Tx_EMR_error_line_count <<- dt[line_count >= 3]
   }
   dt <- dt[line_count == 2 | line_count == 1]
   dt[line_count == 2, c("Tg_line", "tx_line") := tstrsplit(특기사항, split = "\n", fixed = TRUE, fill = NA)]
@@ -15,7 +15,7 @@ my_splilt_lines_for_emr_tx <- function(dt) {
   dt[, tx_line := my_clean_string_edges(tx_line)]
 
   if (input_error_checking_mode == "Y") {
-    emr_tx_line_error <<- dt[!grepl("^[0-9]", dt$tx_line), ]
+    Tx_EMR_error_line <<- dt[!grepl("^[0-9]", dt$tx_line), ]
   }
   dt <- dt[grepl("^[0-9]", dt$tx_line), ]
   
@@ -57,14 +57,14 @@ my_parsing_tx_line <- function(dt) {
 }
 
 
-my_parsing_emr_tx<-function(dt) {
+my_parsing_Tx_EMR<-function(dt) {
   
-  emr_tx <- dt[분류명 %in% c("1st","2nd","3rd","4th","5th","6th","7th","8th","9th")]
-  setnames(emr_tx, "분류명", "Therapy")
-  emr_tx[, 시행일 := as.IDate(등록일, format="%Y-%m-%d")]
+  dt <- dt[분류명 %in% c("1st","2nd","3rd","4th","5th","6th","7th","8th","9th")]
+  setnames(dt, "분류명", "Therapy")
+  dt[, 시행일 := as.IDate(등록일, format="%Y-%m-%d")]
   
-  emr_tx <- my_splilt_lines_for_emr_tx(emr_tx)
+  dt <- my_splilt_lines_for_Tx_EMR(dt)
 
-  return(emr_tx)
+  return(dt)
   
 }
